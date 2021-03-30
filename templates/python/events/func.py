4cf26ceb91d1c43435d32aa522941a0a2f1f9f02
Lance Ball <lball@redhat.com>
2021-03-30 15:01:38 -0400

    fixup: checkpoint
    Signed-off-by: Lance Ball <lball@redhat.com>

--- templates/python/events/func.py
+++ templates/python/events/func.py
@@ -1,17 +1,23 @@
+from werkzeug import Request
+from typing_extensions import TypedDict
 from cloudevents.http import CloudEvent, to_binary
 
-# ---------------------------------------
-# Function template
-# context is a dictionary with the Flask request object and 
-# and a cloud_event key for incoming events
-# ---------------------------------------
-def main(context):
-    # print(f"Method: {context['request'].method}")
-    attributes = {
-      "type": "com.example.fn",
-      "source": "https://example.com/fn"
-    }
-    data = { "message": "Howdy!" }
-    event = CloudEvent(attributes, data)
-    headers, body = to_binary(event)
-    return body, 200, headers
+class Context(TypedDict):
+    request: Request
+    cloud_event: CloudEvent
+
+def main(context: Context):
+  """ 
+  Function template
+  The context parameter contains the Flask request object and any
+  CloudEvent received with the request.
+  """
+  # print(f"Method: {context['request'].method}")
+  attributes = {
+    "type": "com.example.fn",
+    "source": "https://example.com/fn"
+  }
+  data = { "message": "Howdy!" }
+  event = CloudEvent(attributes, data)
+  headers, body = to_binary(event)
+  return body, 200, headers
--- templates/python/http/func.py
+++ templates/python/http/func.py
@@ -1,6 +1,13 @@
-# ---------------------------------------
-# Function template
-# context is a dictionary with the Flask request object 
-# ---------------------------------------
+from werkzeug import Request
+from typing_extensions import TypedDict
+
+class Context(TypedDict):
+    request: Request
+
 def main(context):
-    return { "message": "Howdy!" }, 200
+  """ 
+  Function template
+  The context parameter contains the Flask request object and any
+  CloudEvent received with the request.
+  """
+  return { "message": "Howdy!" }, 200
